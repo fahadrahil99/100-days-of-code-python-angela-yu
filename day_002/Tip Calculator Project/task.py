d8bc1782cd324a9fbf61d80ed64f3af39d467bf9
fahadrahil99 <fahadrahil1999@gmail.com>
2025-12-05 01:01:32 +0530

    Remove empty folders day_001 and day_002

--- day_002/Tip Calculator Project/task.py
+++
@@ -1,6 +0,0 @@
-print("Welcome to the tip calculator!")
-bill = float(input("What was the total bill? $"))
-tip = int(input("What percentage tip would you like to give? 10 12 15 "))
-people = int(input("How many people to split the bill? "))
-a = (bill * (1 + tip/100)/ people)
-print(f"Each person should pay :  {round(a,2)}")
